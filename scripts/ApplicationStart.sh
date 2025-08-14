72a040a23b04eb83e0b40c94d830b76661dd73c2
sumairaa tasnim <sumairaatasnim@outlook.com>
2025-08-14 11:16:36 +0100

    added global path to appstart

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting or reloading Node.js application with PM2..."
+# Explicitly add the npm global bin directory to the PATH
+export PATH=$PATH:/usr/local/bin
 cd /var/www/acebook
-# Use 'pm2 startOrReload' to handle both starting a new process or reloading an existing one
 pm2 startOrReload ./bin/www --name "acebook-app"
-# Save the PM2 process list to ensure it restarts on server reboot
 pm2 save
