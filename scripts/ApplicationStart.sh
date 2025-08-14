36707cdf805c3cac402a00fbb5cb4390a450deb5
sumairaa tasnim <sumairaatasnim@outlook.com>
2025-08-14 11:06:16 +0100

    changes made to appstart again

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Starting Node.js application with PM2..."
+echo "Starting or reloading Node.js application with PM2..."
 cd /var/www/acebook
-/usr/local/bin/pm2 start ./bin/www --name "acebook-app"
+# Use 'pm2 startOrReload' to handle both starting a new process or reloading an existing one
+pm2 startOrReload ./bin/www --name "acebook-app"
+# Save the PM2 process list to ensure it restarts on server reboot
+pm2 save
